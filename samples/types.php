4c6233dc078280f97e81995b73e9c9b45c8621f0
Aleskandr <m.alexandr369@gmail.com>
2020-10-05 23:44:28 +0300

    1

---
+++ samples/types.php
@@ -0,0 +1,14 @@
+<?php
+
+$int = 1;
+$float = 1.0;
+$bool = true;
+$array = [1];
+$object = new stdClass();
+$null = null;
+$resource = fopen(__DIR__. '/basics.php', 'rb');
+fclose ($resource);
+$callable = static function() {
+    return 1;
+};
+var_dump($object, $resource, $callable);
