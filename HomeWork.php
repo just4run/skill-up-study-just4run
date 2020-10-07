aea39080312a8cce2dcf3e75812a353d67dc9c1a
Aleskandr <m.alexandr369@gmail.com>
2020-10-07 18:56:07 +0300

    HOME WORK

---
+++ HomeWork.php
@@ -0,0 +1,44 @@
+<?php
+
+//    error_reporting(E_ALL);
+
+/// _____TASK 1_____
+echo '<p style="background-color: #666; color:#fff; padding: 10px;">Задание 1</p>';
+
+for ($i = 1; $i <= 3; $i++) {
+    $x = $i;
+    if ($x == 1) {
+    echo 1;
+    }
+    if ($x == 2) {
+    echo 2;
+    }
+    if ($x == 3) {
+    echo 3;
+    }
+}
+// 123
+
+echo '<br> <br> <br>';
+
+// _____TASK 2_____
+echo '<p style="background-color: #666; color:#fff; padding: 10px;">Задание 2</p>';
+//$number = (int)$_GET['p1'];
+//if ($number === 0 || $number < 0) {
+//    echo 'Number is zero';
+//} elseif ($number === 1) {
+//    echo 'Number is one';
+//} elseif ($number === 2) {
+//    echo 'Number is two';
+//} elseif ($number % 2 === 0) {
+//    echo "Number {$number} is even";
+//} else {
+//    echo "Number {$number} is odd";
+//}
+//
+//$message = $number % 2 === 0 ? 'even' : 'odd';
+//echo "Number {$number} is {$message}";
+
+echo '<img src="https://www.meme-arsenal.com/memes/b73d0ff9295a4a2fd3f0d8d1d872ebc0.jpg" />';
+echo '<br><p>Не успел, вскорее наверстаю упущенное</p>';
+// switch ($number) --> ugly ternary operators
