bd690910e8d64ad8e8cfe339bed1661f143bf5e0
Aleskandr <m.alexandr369@gmail.com>
2020-10-11 10:29:27 +0300

    Песочница

--- samples/basics.php
+++ samples/basics.php
@@ -1,10 +1,55 @@
 <?php
 
-$test = 0;
-var_dump($test);
+//$test = 0;
+//var_dump($test);
+//
+//$test_2 = 2;
+//var_dump($test_2);
+//
+//$skillUpVariable = 'Hohoho';
+//var_dump($skillUpVariable);
+//
+//$skillUpVariable = 'lalala';
+//var_dump($skillUpVariable);
+//
+//$school = $skillUpVariable;
+//var_dump($school);
+//
+//$testParent = 123;
+//$testChild = &$testParent;
+//var_dump($testParent, $testChild);
+//
+//unset($testChild , $testParent);
+//var_dump($testChild, $testParent);
+//
+//define('TEST_CONST', 555);
+//var_dump(TEST_CONST);
+//
+//defined('TEST_CONST');
+//var_dump(TEST_CONST);
+//
+//$level01 = 'qwerty';
+//$level02 = 'level01';
+//$$$level02 = 222;
+//var_dump($qwerty);
+//
+//${'hello'} = 'OK';
+//var_dump($hello);
+//
+//$qwerty = 123;
+//
+//$test1 = 'test 123 * 444 + $qwerty';
+//var_dump($test1);
+//
+//$test2 = "test \n123 * 444 + $qwerty";
+//var_dump($test2);
+//
+//$test3 = <<<SQL
+//SELECT * FROM data WHERE id = {$qwerty}
+//SQL;
+//var_dump($test3);
+//
+//$test4 = sprintf('test \n123 * 444 + %d', $qwerty);
+//var_dump($test4);
 
-$test_2=2;
-var_dump($test_2);
-
-$skillUpVariable='Hohoho';
-var_dump($skillUpVariable);
+var_dump( __DIR__, __FILE__, __LINE__);
