0141f3e9e2a25f67c7c74d6943ba898d3e306eaa
Aleskandr <m.alexandr369@gmail.com>
2020-10-12 14:00:57 +0300

    added arrays

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php
 
-echo 'Hello World', "\n";
+//echo 'Hello World', "\n";
 
 //var_dump(1 == '1', 1 === '1');
 //var_dump(1 != 2, 1 != 1);
+
+
+$menu = [
+    [
+        'title' => 'Samples',
+        'children' => '',
+
+        [
+            'url' => '/samples/arrays.php',
+            'title' => 'Arrays',
+        ],
+        [
+            'url' => '/samples/basics.php',
+            'title' => 'Basics',
+        ],
+        [
+            'url' => '/samples/branching.php',
+            'title' => 'Branching',
+        ],
+        [
+            'url' => '/samples/html.php',
+            'title' => 'HTML',
+        ],
+        [
+            'url' => '/samples/math.php',
+            'title' => 'Math',
+        ],
+        [
+            'url' => '/samples/types.php',
+            'title' => 'Types',
+        ],
+    ]
+];
+
+var_dump($menu);
---
+++ samples/arrays.php
@@ -0,0 +1,56 @@
+<?php
+
+$a1 = ['test', 'qwerty', 123];
+$a2 = [4 => 'test', 2 => 'qwerty', 244 => 123];
+$a3 = [
+    'key1' => 'Test',
+    'colour' => 'Red',
+    'car' => 'Porshe'
+];
+$groups = [
+    [
+        'id' => 1,
+        'name' => 'PHP basics',
+        'students' => [
+            [
+                'name' => 'Mad Max',
+                'age' => 33
+            ],
+            [
+                'name' => 'Ned Flanders',
+                'age' => 52
+            ]
+        ]
+    ],
+    [
+        'id' => 2,
+        'name' => 'JS basics',
+        'students' => [
+            [
+                'name' => 'Bart Simpson',
+                'age' => 11
+            ],
+            [
+                'name' => 'Bender Rodriges',
+                'age' => 100
+            ]
+        ],
+    ]
+];
+
+//var_dump(
+//    $groups[1]['name'],
+//    $groups[1]['students'][0]['name']);
+
+$groups[0]['students'][] = [
+    'name' => 'Homer Simpson',
+    'age' => 45
+];
+array_push($groups[0]['students'], [
+    'name' => 'Marge Simpson',
+    'age' => 35
+]);
+
+//var_dump($groups);
+
+
